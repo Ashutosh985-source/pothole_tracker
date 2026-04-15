f1f4b23ecda25b7a0c1606a10f13ddf70c5b0a77
Ashutosh985-source <singhashutosh39463@gmail.com>
2026-04-15 21:56:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from modules import gui, db
-import os
+from modules.app import PotholeApp
 
-# Create database folder if not exists
-if not os.path.exists("database"):
-    os.makedirs("database")
-
-db.init_db()
-gui.run_app()
+if __name__ == "__main__":
+    app = PotholeApp()
+    app.run()
